ff39af7eaf1342474e86c66e69288a05f36ed6bc
Sina Zendehroud <zendehroud@physik.fu-berlin.de>
2024-03-01 14:59:48 +0100

    add stdlib dependency

--- src/rot_lange.f90
+++ src/rot_lange.f90
@@ -1,10 +1,14 @@
 module rot_lange
+  use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
   implicit none
   private
 
   public :: say_hello
 contains
+
   subroutine say_hello
-    print *, "Hello, rot_lange!"
+    print *, "sp: 1/3 = ", 1.0_sp/3.0_sp
+    print *, "dp: 1/3 = ", 1.0_dp/3.0_dp
   end subroutine say_hello
+
 end module rot_lange
