9e331404e197b99087d62bb83bcf40dfc63f9c3c
Sina Zendehroud <zendehroud@physik.fu-berlin.de>
2024-03-01 19:28:53 +0100

    fix force update by using dummy object

--- src/integrate.f90
+++ src/integrate.f90
@@ -6,24 +6,26 @@ module integrate
     implicit none
     
     abstract interface
-        function int_func(y) result(f)
-            import :: dp
+        function int_func(y, rb) result(f)
+            import :: dp, t_rigid_body
             real(dp), dimension(18), intent(in) :: y
+            type(t_rigid_body), intent(in) :: rb
             real(dp), dimension(18) :: f
         end function
     end interface
 
 contains
 
-    function func(y) result(grad)
+    function func(y, rb) result(grad)
         real(dp), intent(in) :: y(18)
-        type(t_rigid_body) :: rb
+        type(t_rigid_body), intent(in) :: rb
+        type(t_rigid_body) :: rb_dummy
         real(dp) :: m, dt !, t_end
         real(dp), dimension(3) :: x0, v0, w0
         real(dp), dimension(3, 3) :: I0, R0
         real(dp), dimension(18) :: grad
 
-        dt = 0.001_dp
+        dt = rb%dt
         m = 1.0_dp
         I0(:, 1) = [0.012_dp, 0.0_dp, 0.0_dp]
         I0(:, 2) = [0.0_dp, 0.018_dp, 0.0_dp]
@@ -35,44 +37,28 @@ contains
         R0(:, 3) = [0.0_dp, 0.0_dp, 1.0_dp]
         w0 = [0.0_dp, 0.0_dp, 0.0_dp]
 
-        call init_rigid_body(rb, m, I0, x0, v0, R0, w0, dt)
+        call init_rigid_body(rb_dummy, m, I0, x0, v0, R0, w0, dt)
 
-        call y_to_state(y, rb)
-        ! wrong!
-        call update_force(rb)
-        grad = grad_state_to_y(rb)
+        call y_to_state(y, rb_dummy)
+        rb_dummy%force = rb%force
+        rb_dummy%torque = rb%torque
+        grad = grad_state_to_y(rb_dummy)
     end function func
 
-    function rk_step(f, y, dt) result(step)
+    function rk_step(f, y, rb) result(step)
         real(dp), intent(in) :: y(18)
-        real(dp), intent(in) :: dt
+        type(t_rigid_body), intent(in) :: rb
+        real(dp) :: dt
         real(dp) :: step(18)
         procedure(int_func) :: f
         real(dp), dimension(18) :: k1, k2, k3, k4
 
-        k1 = f(y)
-        k2 = f(y + 0.5_dp * dt * k1)
-        k3 = f(y + 0.5_dp * dt * k2)
-        k4 = f(y + dt * k3)
+        dt = rb%dt
+        k1 = f(y, rb)
+        k2 = f(y + 0.5_dp * dt * k1, rb)
+        k3 = f(y + 0.5_dp * dt * k2, rb)
+        k4 = f(y + dt * k3, rb)
         step = y + dt / 6.0_dp * (k1 + 2.0_dp * k2 + 2.0_dp * k3 + k4)
     end function
-
-    ! subroutine rk4(f, y, t, omega, phi, rb, dt)
-    !     real(dp), intent(inout) :: y(18), t
-    !     real(dp), intent(in) :: dt
-    !     real(dp), dimension(:,:), intent(inout) :: omega, phi
-    !     type(t_rigid_body), intent(in) :: rb
-    !     procedure(int_func) :: f
-    !     integer :: i
-        
-    !     omega(:, 1) = rb%angular_velocity
-    !     phi(:, 1) = rb%angular_displacement
-    !     do i = 1, iend
-    !         y(:, i+1) = rk_step(f, y(:, i), dt)
-    !         omega(:, i+1) = rb%angular_velocity
-    !         phi(:, i+1) = rb%angular_displacement
-    !     end do
-    ! end subroutine
-
     
 end module integrate
